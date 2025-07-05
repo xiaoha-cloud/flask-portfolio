0639992ec8fb1426f25d213d86a95a0beb9abeb7
Ace Perez <acenathen.perez@gmail.com>
2025-07-05 22:39:07 +0100

    adding deployment script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ echo "Pulling latest code from GitHub..."
 git fetch && git reset origin/main --hard
 
 echo "Activating virtual environment..."
-source venv/bin/activate  # Ensure this venv exists; or adjust path
+source venv/bin/activate
 
 echo "Installing dependencies..."
 pip install -r requirements.txt
