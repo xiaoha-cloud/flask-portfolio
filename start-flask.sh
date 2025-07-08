aec3536d0af6dee61bdc5a5ebcb7bb1ade28e919
Xiaoha <blairjade183@gmail.com>
2025-07-08 14:20:35 +0100

    fix: update deployment scripts for tmux and debugging

--- start-flask.sh
+++ start-flask.sh
@@ -3,4 +3,5 @@ cd ~/flask-portfolio
 source python3-virtualenv/bin/activate
 export FLASK_APP=app
 export FLASK_ENV=production
-python -m flask run --host=0.0.0.0 --port=5000 > flask.log 2>&1 
+python -m flask run --host=0.0.0.0 --port=5000
+exec bash
