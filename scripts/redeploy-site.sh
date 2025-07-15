0a73324590d359ad58ada343743c932bf04e9fb7
Ace Perez <acenathen.perez@gmail.com>
2025-07-15 11:06:30 +0000

    changed script to replace tmux as a server

--- scripts/redeploy-site.sh
+++ scripts/redeploy-site.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
-set -e
-
-echo "Killing all tmux sessions..."
-tmux ls 2>/dev/null | awk -F: '{print $1}' | xargs -r -I {} tmux kill-session -t {}
+LOGFILE="errors/\$(date +\"%Y-%m-%d_%H-%M-%S\")"
 
 echo "Going to project directory..."
 cd ~/flask-portfolio
@@ -17,7 +14,7 @@ source python3-virtualenv/bin/activate
 echo "Installing dependencies..."
 pip install -r requirements.txt
 
-echo "Starting Flask app in tmux (detached)..."
-tmux new-session -d -s flask_server "bash -c 'cd ~/flask-portfolio && source python3-virtualenv/bin/activate && mkdir -p errors && LOGFILE=errors/\$(date +\"%Y-%m-%d_%H-%M-%S\")-flask.log && flask run --host=0.0.0.0 --port=5001 2>&1 | tee \$LOGFILE'"
+echo "Restarting myportfolio service..."
+sudo systemctl restart myportfolio 2>> $LOGFILE
 
 echo "Site redeployed successfully!"
