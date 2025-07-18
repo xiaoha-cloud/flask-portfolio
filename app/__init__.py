deabb8f3fc2997ae2f8fce6513f08b5ebb115325
Ace Perez <acenathen.perez@gmail.com>
2025-07-18 14:46:25 +0100

    added the test cases, for exercise 1

--- app/__init__.py
+++ app/__init__.py
@@ -243,3 +243,14 @@ def delete_timeline_post(post_id):
         return jsonify({'result': 'Deleted'}), 200
     else:
         return jsonify({'error': 'Not found'}), 404
+
+if os.getenv("TESTING") == "true":
+    print("Running in test mode")
+    mydb = SqliteDatabase('file:memory?mode=memory&cache=shared', uri=True)
+else:
+    mydb = MySQLDatabase(os.getenv("MYSQL_DATABASE"),
+        user=os.getenv("MYSQL_USER"),
+        password=os.getenv("MYSQL_PASSWORD"),
+        host=os.getenv("MYSQL_HOST"),
+        port=3306
+    )
